c1631a34ec9fd0b4b2a9e8aa2868e1ce88c40a36
Nicholas Bardy <nicholasbardy@gmail.com>
2017-04-14 23:23:50 -0400

    Fix build process

--- gh-deploy.sh
+++ gh-deploy.sh
@@ -2,6 +2,6 @@ git checkout -B gh-pages
 npm run build
 git add -f dist
 git commit -am "Rebuild website"
-git filter-branch -f --prune-empty --subdirectory-filter build
+git filter-branch -f --prune-empty --subdirectory-filter dist
 git push -f origin gh-pages
 git checkout -
